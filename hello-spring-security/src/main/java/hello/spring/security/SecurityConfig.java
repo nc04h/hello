98cf7d9acf76ef70a672a4d3b71c09aa5ef2c169
A U Thor <nbaxjnua@gmail.com>
2016-10-17 01:03:55 +0300

    removes unused files

--- hello-spring-security/src/main/java/hello/spring/security/SecurityConfig.java
+++ hello-spring-security/src/main/java/hello/spring/security/SecurityConfig.java
@@ -1,36 +1,21 @@
 package hello.spring.security;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
-import org.springframework.web.filter.GenericFilterBean;
 
 import hello.spring.security.basic.MyBasicAuthenticationProvider;
-import hello.spring.security.digest.MyDigestAuthenticationProvider;
 import hello.spring.security.digest.MyDigestUserDetailService;
 
 @EnableWebSecurity
