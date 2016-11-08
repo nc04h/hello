3ce447cb7e0503dd31c84d129890628c5a674e53
A U Thor <nbaxjnua@gmail.com>
2016-11-08 19:09:11 +0300

    oauth

--- hello-spring-security/src/main/java/hello/spring/security/oauth/MyOAuthController.java
+++ hello-spring-security/src/main/java/hello/spring/security/oauth/MyOAuthController.java
@@ -1,9 +1,11 @@
 package hello.spring.security.oauth;
 
 import org.apache.log4j.Logger;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/oauth")
@@ -11,10 +13,11 @@ public class MyOAuthController {
 
 	private static final Logger log = Logger.getLogger(MyOAuthController.class);
 
+	@PreAuthorize("hasRole('ROLE_OAUTH_USER')")
 	@RequestMapping(path = "/auth", method = RequestMethod.GET)
-	public String auth() {
+	public @ResponseBody String auth() {
 		// TODO
-	    log.debug("auth");
+		log.debug("auth");
 		return "index";
 	}
 }
