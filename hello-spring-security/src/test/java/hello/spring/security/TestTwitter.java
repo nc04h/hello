65be9944566d52cfd18eaf5d1a1f15a98701e20e
A U Thor <nbaxjnua@gmail.com>
2016-11-15 01:44:22 +0300

    q

--- hello-spring-security/src/test/java/hello/spring/security/TestTwitter.java
+++ hello-spring-security/src/test/java/hello/spring/security/TestTwitter.java
@@ -1,10 +1,14 @@
 package hello.spring.security;
 
+import java.util.List;
+
 import org.apache.http.HttpHost;
 import org.apache.http.client.HttpClient;
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class TestTwitter extends TestAbstract {
@@ -19,7 +23,21 @@ public class TestTwitter extends TestAbstract {
 		final HttpHost host = new HttpHost("localhost", 1234);
 		HttpClient httpClient = httpClient(login, password, host);
 		RestTemplate restTemplate = new RestTemplate(new TestRequestFactory(httpClient, host));
-		String response = restTemplate.exchange(url, HttpMethod.POST, null, String.class).getBody();
+		
+		ResponseEntity<Object> responseEntity = restTemplate.exchange(url, HttpMethod.POST, null, Object.class);
+		Object response = responseEntity.getBody();
 		log.debug(response);
+		HttpHeaders headers = responseEntity.getHeaders();
+		log.debug(headers);
+		log.debug(responseEntity.getStatusCode());
+		List<String> locations = headers.get("Location");
+		log.debug(locations);
+		locations.stream().findFirst().ifPresent(location -> {
+			log.debug(location);
+			ResponseEntity<String> responseEntity2 = restTemplate.getForEntity(location, String.class);
+			log.debug(responseEntity2);
+		});
+		
+		
 	}
 }
